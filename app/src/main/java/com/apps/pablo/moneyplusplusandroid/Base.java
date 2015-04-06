f71ed166651736f26705d005f236dde9413283f1
Pablo1994 <jpaz199448@gmail.com>
2015-04-06 11:21:20 -0600

    Solo falta la interfaz del ingreso periódico.

--- app/src/main/java/com/apps/pablo/moneyplusplusandroid/Base.java
+++ app/src/main/java/com/apps/pablo/moneyplusplusandroid/Base.java
@@ -3,6 +3,7 @@ package com.apps.pablo.moneyplusplusandroid;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
@@ -21,10 +22,10 @@ public class Base extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String TAG = "MoneyPlus";
-        if(this.db == null) {
+        if(db == null) {
             try {
                 manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
-                this.db = manager.getDatabase("money_db");
+                db = manager.getDatabase("money_db");
                 Log.i(TAG, "Database created!");
             } catch (IOException e) {
                 Log.e(TAG, "Cannot create database", e);
@@ -33,5 +34,8 @@ public class Base extends ActionBarActivity {
                 Log.e(TAG, e.getMessage());
             }
         }
+    }
+    public void Mensaje(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/apps/pablo/moneyplusplusandroid/tabIngresoDiario.java
+++ app/src/main/java/com/apps/pablo/moneyplusplusandroid/tabIngresoDiario.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by pablo on 05/04/15.
