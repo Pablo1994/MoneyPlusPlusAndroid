601531a787ba7927afdc493a1cd647d46c4402e4
Pablo1994 <jpaz199448@gmail.com>
2015-04-07 21:17:39 -0600

    Todos los inserts listos, sigue crear los objetos y llamar a los inserts en la interfaz.

--- app/src/main/java/com/apps/pablo/moneyplusplusandroid/DBManager.java
+++ app/src/main/java/com/apps/pablo/moneyplusplusandroid/DBManager.java
@@ -18,6 +18,8 @@ public class DBManager {
     public static final String MONTO = "monto";
     public static final String DESCRIPCION = "descripcion";
     public static final String FECHA = "fecha";
+    public static final String DIA = "dia";
+    public static final String FRECUENCIA = "frecuencia";
 
     public static final String TABLA_ING_PURO = "ingreso_puro";
     public static final String ID_ING_PURO = "ingPuroId";
@@ -25,11 +27,37 @@ public class DBManager {
     public static final String INGRESO_PURO = "CREATE TABLE " + TABLA_ING_PURO + " (" + ID_ING_PURO +
             " INTEGER primary key autoincrement, " + MONTO + " INTEGER, " + DESCRIPCION +
             " TEXT, " + FECHA + " TEXT);";
-    public static final String INGRESO_PER_DIA = "CREATE TABLE ingreso_per_dia (ingPerDiaId INTEGER primary key autoincrement, monto INTEGER, descripcion TEXT, dia TEXT, frecuencia TEXT);";
-    public static final String INGRESO_DIARIO = "CREATE TABLE ingreso_diario (ingDiarioId INTEGER primary key autoincrement, monto INTEGER, descripcion TEXT);";
-    public static final String DIAS = "CREATE TABLE dias (dia TEXT, idIngDiario INT, FOREIGN KEY(idIngDiario) REFERENCES ingreso_diario(ingDiarioId));";
-    public static final String INGRESO_PER_FECHA = "CREATE TABLE ingreso_per_fecha (ingPerFechaId INTEGER primary key autoincrement, monto INTEGER, descripcion TEXT, frecuencia TEXT);";
-    public static final String FECHAS = "CREATE TABLE fechas (fecha TEXT, idIngPerFec INT, FOREIGN KEY(idIngPerFec) REFERENCES ingreso_per_fecha(ingPerFechaId));";
+
+    public static final String TABLA_ING_DIA = "ingreso_per_dia";
+    public static final String ID_ING_DIA = "ingPerDiaId";
+
+    public static final String INGRESO_PER_DIA = "CREATE TABLE " + TABLA_ING_DIA + " (" + ID_ING_DIA + " INTEGER primary key autoincrement, " +
+            MONTO + " INTEGER, " + DESCRIPCION + " TEXT, " + DIA + " TEXT, " + FRECUENCIA + " TEXT);";
+
+    public static final String TABLA_ING_DIARIO = "ingreso_diario";
+    public static final String ID_ING_DIARIO = "ingDiarioId";
+
+    public static final String INGRESO_DIARIO = "CREATE TABLE " + TABLA_ING_DIARIO + " (" + ID_ING_DIARIO + " INTEGER primary key autoincrement, " +
+            MONTO + " INTEGER, " + DESCRIPCION + " TEXT);";
+
+    public static final String TABLA_DIAS = "dias";
+    public static final String FK_ING_DIARIO = "idIngDiario";
+
+    public static final String DIAS = "CREATE TABLE " + TABLA_DIAS + " (" + DIA + " TEXT, " + FK_ING_DIARIO +
+            " INT, FOREIGN KEY(" + FK_ING_DIARIO+ ") REFERENCES " +
+            TABLA_ING_DIARIO + "(" + ID_ING_DIARIO + "));";
+
+    public static final String TABLA_ING_FECHA = "ingreso_per_fecha";
+    public static final String ID_ING_FECHA = "ingPerFechaId";
+
+    public static final String INGRESO_PER_FECHA = "CREATE TABLE " + TABLA_ING_FECHA + " (" + ID_ING_FECHA + " INTEGER primary key autoincrement, " +
+            MONTO + " INTEGER, " + DESCRIPCION + " TEXT, " + FRECUENCIA + " TEXT);";
+
+    public static final String TABLA_FECHAS = "fechas";
+    public static final String FK_ING_FECHAS = "idIngPerFec";
+
+    public static final String FECHAS = "CREATE TABLE " + TABLA_FECHAS + " ("+ FECHA + " TEXT, " + FK_ING_FECHAS +
+            " INT, FOREIGN KEY(" + FK_ING_FECHAS + ") REFERENCES " + TABLA_ING_FECHA + "(" + FK_ING_FECHAS +"));";
     public static String ingresos [] = {INGRESO_PURO,INGRESO_DIARIO,INGRESO_PER_DIA,INGRESO_PER_FECHA,DIAS,FECHAS};
 
     protected static DBHelper helper = null;
@@ -53,13 +81,60 @@ public class DBManager {
         return db.insert(TABLA_ING_PURO, null, valoresIngresoPuro(i)) != -1;
     }
 
-    public void insertar(IngresoDiario i){
-
+    public ContentValues valoresIngresoDiario(IngresoDiario i){
+        ContentValues values = new ContentValues();
+        values.put(MONTO, i.getMonto());
+        values.put(DESCRIPCION, i.getDescripcion());
+        return values;
     }
-    public void insertar(IngresoPeriodicoDia i){
-
+    public boolean insertar(IngresoDiario i){
+        long id = db.insert(TABLA_ING_DIARIO, null, valoresIngresoDiario(i));
+        if(id != -1){
+            ContentValues valoresDias = new ContentValues();
+            for(String s : i.getDias()){
+                valoresDias.put(DIA,s);
+                valoresDias.put(FK_ING_DIARIO,id);
+                if(db.insert(DIAS,null,valoresDias) == -1)
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
     }
-    public void insertar(IngresoPeriodicoFecha i){
 
+    public ContentValues valoresIngresoPerDia (IngresoPeriodicoDia i){
+        ContentValues values = new ContentValues();
+        values.put(MONTO, i.getMonto());
+        values.put(DESCRIPCION, i.getDescripcion());
+        values.put(DIA,i.getDia());
+        values.put(FRECUENCIA,i.getFrecuencia());
+        return values;
+    }
+    public boolean insertar(IngresoPeriodicoDia i){
+        return db.insert(TABLA_ING_DIA, null, valoresIngresoPerDia(i)) != 1;
+    }
+    public ContentValues valoresIngresoFechas(IngresoPeriodicoFecha i){
+        ContentValues values = new ContentValues();
+        values.put(MONTO, i.getMonto());
+        values.put(DESCRIPCION, i.getDescripcion());
+        values.put(FRECUENCIA, i.getFrecuencia());
+        return values;
+    }
+    public boolean insertar(IngresoPeriodicoFecha i){
+        long id = db.insert(TABLA_ING_DIARIO, null, valoresIngresoFechas(i));
+        if(id != -1){
+            ContentValues valoresFechas = new ContentValues();
+            for(int d : i.getFechas()){
+                String s = String.valueOf(d);
+                valoresFechas.put(DIA,s);
+                valoresFechas.put(FK_ING_DIARIO,id);
+                if(db.insert(DIAS,null,valoresFechas) == -1)
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
     }
 }
